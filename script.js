50f7945a29735a6cd7992933addb015867c279d1
oleoot <oleoot28@gmail.com>
2020-05-12 15:02:00 +0300

    User change + some styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const key = "AIzaSyAsMk24GkL5r7Z35D04-IdYIPAa6HyRPoQ";
-const id = "UCi4s3kO1-xxCDaw8yX56umw"
+const id = "UCFxhJpcIvqZIcOBarAZG9Gw"
 const api_url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${id}&key=${key}`;
 const counter_wrap = document.querySelector('#live-count-wrap')
 const counter = document.querySelector('#live-sub-count')
